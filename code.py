43a002ab54d11500a7ce3cf10ff608320f86a681
lgabs <luan.fernandes@redealumni.com>
2020-11-23 11:33:06 -0300

    black everything

--- code.py
+++ code.py
@@ -1,12 +1,17 @@
 import numpy as np
 
-def somar(a,   b):
-    answer = a   +    b
+
+def somar(a, b):
+    answer = a + b
+
     return answer
 
-def subtract(a,   b):
-    answer = a   -    b
+
+def subtract(a, b):
+    answer = a - b
+
     return answer
+
 
 def inner_product(v, w):
     return np.dot(v, w)
--- tests/test_code.py
+++ tests/test_code.py
@@ -2,19 +2,19 @@ from code import somar, subtract, inner_product
 import numpy as np
 import unittest
 
+
 class CodeTestCase(unittest.TestCase):
     def test_somar(self):
-        self.assertEqual(somar(1,2), 3)
+        self.assertEqual(somar(1, 2), 3)
 
     def test_subtract(self):
-        self.assertEqual(subtract(2,1), 1)
+        self.assertEqual(subtract(2, 1), 1)
 
     def test_inner_product(self):
-        v = np.array([9,10])
+        v = np.array([9, 10])
         w = np.array([11, 12])
-        result = 9*11 + 10*12
-        self.assertEqual(inner_product(v,w), result)
-
+        result = 9 * 11 + 10 * 12
+        self.assertEqual(inner_product(v, w), result)
 
 
 if __name__ == "__main__":
